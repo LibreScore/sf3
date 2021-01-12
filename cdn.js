e798f210a83849aabc89a322dd78285679b43a88
Xmader <xmader@outlook.com>
2021-01-12 11:10:35 -0500

    release v0.6.0
    rename the asset file to be with the `wasm` file extension
    
    web servers can only automatically compress some file formats

--- cdn.js
+++ cdn.js
@@ -5,4 +5,4 @@ const CDN_PROVIDER = "https://cdn.jsdelivr.net/npm"
 const URL_PREFIX = `${CDN_PROVIDER}/${name}@${version}` // https://cdn.jsdelivr.net/npm/@librescore/sf3@ver
 
 // file URLs
-module.exports.FluidR3Mono = `${URL_PREFIX}/FluidR3Mono_GM.sf3`
+module.exports.FluidR3Mono = `${URL_PREFIX}/FluidR3Mono_GM.sf3.wasm`
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 
 const path = require("path")
 
-module.exports.FluidR3Mono = path.join(__dirname, "FluidR3Mono_GM.sf3")
+module.exports.FluidR3Mono = path.join(__dirname, "FluidR3Mono_GM.sf3.wasm")
