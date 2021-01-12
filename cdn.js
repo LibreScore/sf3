48229a48ebbda0484007ed8399745b3f0f0f6f60
Xmader <xmader@outlook.com>
2021-01-12 11:04:15 -0500

    release v0.5.0
    download the asset from CDN

---
+++ cdn.js
@@ -0,0 +1,8 @@
+
+const { name, version } = require("./package.json")
+
+const CDN_PROVIDER = "https://cdn.jsdelivr.net/npm"
+const URL_PREFIX = `${CDN_PROVIDER}/${name}@${version}` // https://cdn.jsdelivr.net/npm/@librescore/sf3@ver
+
+// file URLs
+module.exports.FluidR3Mono = `${URL_PREFIX}/FluidR3Mono_GM.sf3`
